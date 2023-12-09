ceb1dedb8695f372cae0e2e77639c4321b281baf
Maotou <93722462+maotou-spy@users.noreply.github.com>
2023-12-09 14:33:49 +0700

    [maotou][update database]

--- Database/WareHouseManagementDB.sql
+++ Database/WareHouseManagementDB.sql
@@ -4,36 +4,36 @@ USE PRN221_Fall23_3W_WareHouseManagement
 -- Tạo bảng Category
 CREATE TABLE Category (
     CategoryId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-    CategoryCode VARCHAR(255) NOT NULL,
-    Name NVARCHAR(255) NOT NULL,
+    CategoryCode VARCHAR(20) NOT NULL,
+    Name NVARCHAR(50) NOT NULL,
     Status INT NOT NULL
 );
 
 -- Tạo bảng StorageArea
 CREATE TABLE StorageArea (
     AreaId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-    AreaCode VARCHAR(255) NOT NULL,
-    AreaName NVARCHAR(255) NOT NULL,
+    AreaCode VARCHAR(20) NOT NULL,
+    AreaName NVARCHAR(50) NOT NULL,
     Status INT NOT NULL
 );
 
 -- Tạo bảng Account
 CREATE TABLE Account (
     AccountId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-    AccountCode VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) NOT NULL,
-    Name NVARCHAR(255) NOT NULL,
-    Password VARCHAR(255) NOT NULL,
+    AccountCode VARCHAR(20) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    Name NVARCHAR(50) NOT NULL,
+    Password VARCHAR(50) NOT NULL,
     Role INT NOT NULL,
-    Phone VARCHAR(255),
+    Phone VARCHAR(50),
     Status INT NOT NULL
 );
 
 -- Tạo bảng Partner
 CREATE TABLE Partner (
     PartnerId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-    PartnerCode VARCHAR(255) NOT NULL,
-    Name NVARCHAR(255) NOT NULL,
+    PartnerCode VARCHAR(20) NOT NULL,
+    Name NVARCHAR(50) NOT NULL,
     Status INT NOT NULL
 );
 
@@ -42,9 +42,9 @@ CREATE TABLE Lot (
     LotId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
     AccountId INT NOT NULL,
     PartnerId INT NOT NULL,
-    LotCode VARCHAR(255) NOT NULL,
+    LotCode VARCHAR(20) NOT NULL,
     DateIn DATE NOT NULL,
-    Note NVARCHAR(255),
+    Note TEXT,
     Status INT NOT NULL,
     FOREIGN KEY (AccountId) REFERENCES Account(AccountId),
     FOREIGN KEY (PartnerId) REFERENCES Partner(PartnerId)
@@ -56,7 +56,7 @@ CREATE TABLE StockOut (
     AccountId INT NOT NULL,
     PartnerId INT NOT NULL,
     DateOut DATE NOT NULL,
-    Note NVARCHAR(255),
+    Note TEXT,
     Status INT NOT NULL,
     FOREIGN KEY (AccountId) REFERENCES Account(AccountId),
     FOREIGN KEY (PartnerId) REFERENCES Partner(PartnerId)
@@ -67,8 +67,8 @@ CREATE TABLE Product (
     ProductId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
     CategoryId INT NOT NULL,
     AreaId INT NOT NULL,
-    ProductCode VARCHAR(255) NOT NULL,
-    Name NVARCHAR(255),
+    ProductCode VARCHAR(20) NOT NULL,
+    Name NVARCHAR(50),
     Quantity INT NOT NULL,
     Status INT NOT NULL,
     FOREIGN KEY (CategoryId) REFERENCES Category(CategoryId),
