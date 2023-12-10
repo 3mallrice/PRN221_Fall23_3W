4d952cb0b200c966dfd2e3fc2b7e5515217a8a77
Đức Bùi <117875129+baemgao@users.noreply.github.com>
2023-12-10 09:40:23 +0700

    [Ducbui][update project]

---
+++ BL3w_GroupProject_WareHourse/BusinessObject/Models/StockOutDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObject.Models
+{
+    public partial class StockOutDetail
+    {
+        public int StockOutDetailId { get; set; }
+        public int ProductId { get; set; }
+        public int StockOutId { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual Product Product { get; set; } = null!;
+        public virtual StockOut StockOut { get; set; } = null!;
+    }
+}
---
+++ BL3w_GroupProject_WareHourse/BusinessObject/Models/StorageArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessObject.Models
+{
+    public partial class StorageArea
+    {
+        public StorageArea()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int AreaId { get; set; }
+        public string AreaCode { get; set; } = null!;
+        public string AreaName { get; set; } = null!;
+        public int Status { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
