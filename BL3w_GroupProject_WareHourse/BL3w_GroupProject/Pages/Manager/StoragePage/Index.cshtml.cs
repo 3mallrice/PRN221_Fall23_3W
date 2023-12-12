bf45641b81957663ab0c8f92744c03ea3ac82292
codedudu <124663016+Code-dudu@users.noreply.github.com>
2023-12-12 15:37:38 +0700

    [Trong][Add layout manager"category,storagArea]

---
+++ BL3w_GroupProject_WareHourse/BL3w_GroupProject/Pages/Manager/StoragePage/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject.Models;
+
+namespace BL3w_GroupProject.Pages.Manager.StoragePage
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BusinessObject.Models.PRN221_Fall23_3W_WareHouseManagementContext _context;
+
+        public IndexModel(BusinessObject.Models.PRN221_Fall23_3W_WareHouseManagementContext context)
+        {
+            _context = context;
+        }
+
+        public IList<StorageArea> StorageArea { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.StorageAreas != null)
+            {
+                StorageArea = await _context.StorageAreas.ToListAsync();
+            }
+        }
+    }
+}
