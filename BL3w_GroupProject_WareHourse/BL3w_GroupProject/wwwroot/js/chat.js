bd40b39d16b66fbc14d989afd55c7df606207409
Maotou <93722462+maotou-spy@users.noreply.github.com>
2023-12-14 06:58:09 +0700

    [maotou][update chat]

--- BL3w_GroupProject_WareHourse/BL3w_GroupProject/wwwroot/js/chat.js
+++ BL3w_GroupProject_WareHourse/BL3w_GroupProject/wwwroot/js/chat.js
@@ -6,12 +6,32 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
+    var messagesList = document.getElementById("messagesList");
+
+    // Create a new list item element
     var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
-    // We can assign user-supplied strings to an element's textContent because it
-    // is not interpreted as markup. If you're assigning in any other way, you 
-    // should be aware of possible script injection concerns.
-    li.textContent = `${user} : ${message}`;
+
+    // Assign a class based on the index of the list item
+    var index = messagesList.children.length;
+    if (index % 2 !== 0) {
+        li.classList.add("text-success");
+    } else {
+        li.classList.add("text-info");
+    }
+
+    // Set the content of the list item
+    li.textContent = `${user}: ${message}`;
+
+    // Append the list item to the messagesList
+    messagesList.appendChild(li);
+
+    // Check if the messagesList is scrolled to the bottom
+    var isScrolledToBottom = messagesList.scrollHeight - messagesList.clientHeight <= messagesList.scrollTop + 1;
+
+    // If scrolled to the bottom, auto-scroll to show the latest message
+    if (isScrolledToBottom) {
+        messagesList.scrollTop = messagesList.scrollHeight;
+    }
 });
 
 connection.start().then(function () {
